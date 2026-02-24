053824a140891911292b848042b7c4f345649da5
kemily77777 <kemilyberti92@gmail.com>
2026-02-24 09:18:06 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class PackKekeApp(App):
+    def build(self):
+        return Label(text="Pack KeKe funcionando 🔥")
+
+PackKekeApp().run()
